cc908a1fd6af2befd8725cf6aa6cc7ea64b7fb19
stevenks17 <stevenks17@gmail.com>
2020-07-31 14:06:56 -0400

    send/recieve/store and show array values

--- src/App.js
+++ src/App.js
@@ -3,17 +3,28 @@ import './App.css';
 
 function App() {
   const [input, setInput] = useState('')
+  const [messages, setMessages] = useState([])
+  console.log(input)
+  console.log(messages)
+  const sendMessage = (event) => {
+      // all sent message logic goes here
+      setMessages([...messages, input])
+      setInput('')
+  }
   return (
     <div className="App">
       <h1> An app for those talkative few </h1>
 
       <input value={input} onChange={event => setInput(event.target.value)}></input>
-      <button>Send Message</button> 
-      {/* input field */}
-      {/* button */}
+      <button onClick={sendMessage}>Send Message</button> 
 
 
       {/* messages */}
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+      }
 
     </div>
   );
