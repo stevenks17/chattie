6942a45626a03b6507d31793c059098f35963a5f
github username <github email address>
2020-08-04 16:40:39 -0400

    added username and prompt to claim messages

--- src/App.js
+++ src/App.js
@@ -1,32 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { Button } from '@material-ui/core';
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
+import Message from './Message';
 
 
 function App() {
   const [input, setInput] = useState('')
-  const [messages, setMessages] = useState([])
-  console.log(input)
-  console.log(messages)
+  const [messages, setMessages] = useState([
+  {username: 'Steve', text: 'Hello'},
+  {username: 'Celine', text: 'Goodbye'}
+  ])
+  const [userName, setUsername] = useState('')
+
+useEffect(() => {
+  // code is run here
+  // if blank code is executed once on app.js load
+  // if we have a variable added it runs everytime its changed 
+  setUsername(prompt('Please enter your name'))
+}
+, []) // condition
+
   const sendMessage = (event) => {
       // all sent message logic goes here
       event.preventDefault()
-      setMessages([...messages, input])
+      setMessages([
+        ...messages, { userName: userName, text: input }
+      ])
       setInput('')
   }
   return (
     <div className="App">
-      <h1> An app for those talkative few </h1>
+      <h1> An app for those chatty people </h1>
+      <h2> Welcome {userName} </h2>
+
+
 
       <form>
-        <input value={input} onChange={event => setInput(event.target.value)}></input>
-        <Button disabled={!input} variant= "contained" color="primary" type='submit' onClick={sendMessage}>Send Message</Button> 
+        <FormControl>
+          <InputLabel >Enter a message...</InputLabel>
+          <Input input value={input} onChange={event => setInput(event.target.value)}/>
+          <Button disabled={!input} variant= "contained" color="primary" type='submit' onClick={sendMessage}>Send Message</Button> 
+        </FormControl>
       </form>
 
       {/* messages */}
       {
         messages.map(message => (
-          <p>{message}</p>
+          <Message username={message.username} text={message.text} />
         ))
       }
 
