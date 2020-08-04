7fbc6fd6ade57e4069dc0abfc6b8070f784b4f52
github username <github email address>
2020-08-04 16:41:12 -0400

    added message component to style using cards from material ui

---
+++ src/Message.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Card, CardContent, Typography } from '@material-ui/core'
+
+function Message(props) {
+    return (
+            <Card>
+                <CardContent>
+                    <Typography 
+                        color="white"
+                        variant="h5"
+                        component="h2"
+                    >
+                        {props.username}: {props.text}          
+                    </Typography>
+                </CardContent>
+            </Card>
+        
+    )
+}
+
+export default Message
