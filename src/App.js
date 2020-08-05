d22e29a0e81e1215d2cfdb10730cf44429994377
stevenks17 <stevenks17@gmail.com>
2020-08-04 20:24:13 -0400

    Db connection established

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import Message from './Message';
+import firebase from "firebase"
+import db from "./firebase"
 
 
 function App() {
   const [input, setInput] = useState('')
-  const [messages, setMessages] = useState([
-  {userName: 'Steve', text: 'Hello'},
-  {userName: 'Celine', text: 'Goodbye'}
-  ])
+  const [messages, setMessages] = useState([])
   const [userName, setUsername] = useState('')
 
-useEffect(() => {
-  // code is run here
-  // if blank code is executed once on app.js load
-  // if we have a variable added it runs everytime its changed 
-  setUsername(prompt('Please enter your name'))
-}
-, []) // condition
+  useEffect(() => {
+    // when loaded run this once
+    db.collection("messages").onSnapshot(snapshot => { 
+      setMessages(snapshot.docs.map(doc => doc.data()))
+    })
+  }, [])
+
+
+  useEffect(() => {
+    // code is run here
+    // if blank code is executed once on app.js load
+    // if we have a variable added it runs everytime its changed 
+    setUsername(prompt('Please enter your name'))
+  }
+  , []) // condition
 
   const sendMessage = (event) => {
       // all sent message logic goes here
@@ -46,7 +53,7 @@ useEffect(() => {
       {/* messages */}
       {
         messages.map(message => (
-          <Message username={message.userName} text={message.text} />
+          <Message username={userName} message={message} />
         ))
       }
 
